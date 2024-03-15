430e4afcb1b19e9191efded9b296cfd384b85e94
pkji <pk@gmail.com>
2024-03-15 16:42:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      <h1 className='text-[50px] text-red-900 font-bold'>priyanka sharma</h1>
+      <h1 className='text-[50px] text-red-500 font-bold'>anurag sharma</h1>
+      <h1 className='text-[50px] text-blue-200 font-bold'>pinki sharma</h1>
+      <h1 className='text-[90px] text-green-900 font-bold'>hello-world</h1>
+      
+    </div>
+  );
+}
+
+export default App;
